7ee17fb769ca75f327023ee15901be791822638a
Sahil Chhabra <sahil2921@gmail.com>
2020-01-07 23:19:31 +0100

    Fixed Workspace path to parent docker

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,6 +6,6 @@ echo "################################"
 
 # Example - ./jenkins/build/mvn.sh mvn -B -DskipTests clean package
 
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/Users/sahilchhabra/jenkins/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm -v $WORKSPACE/java-app:/app -v $WORKSPACE/m2:/root/.m2 -w /app maven:3-alpine "$@"
--- jenkins/test/mvn-test.sh
+++ jenkins/test/mvn-test.sh
@@ -5,6 +5,6 @@ echo "########## Testing JAR #########"
 echo "################################"
 
 # Example - ./jenkins/test/mvn-test.sh mvn test
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/Users/sahilchhabra/jenkins/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm -v $WORKSPACE/java-app:/app -v $WORKSPACE/m2:/root/.m2 -w /app maven:3-alpine "$@"
