7b65439bca29547b298fcb3eb714a6079eaa18e6
Sahil Chhabra <sahil2921@gmail.com>
2020-01-07 22:25:34 +0100

    Fixed ssh key path

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,8 +4,8 @@ echo maven-project-4-jenkins > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i ~/jenkins/jenkins_home/ssh-keys/prod /tmp/.auth prod_user@prod-host:/tmp/.auth
+scp -i /var/jenkins_home/ssh-keys/prod /tmp/.auth prod_user@prod-host:/tmp/.auth
 
-scp -i ~/jenkins/jenkins_home/ssh-keys/prod ./jenkins/deploy/publish.sh prod_user@prod-host:/tmp/publish
+scp -i /var/jenkins_home/ssh-keys/prod ./jenkins/deploy/publish.sh prod_user@prod-host:/tmp/publish
 
-ssh -i ~/jenkins/jenkins_home/ssh-keys/prod prod_user@prod-host "/tmp/publish"
+ssh -i /var/jenkins_home/ssh-keys/prod prod_user@prod-host "/tmp/publish"
